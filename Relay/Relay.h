ad95ebe7cfb0dbfde9abd6d76eb156a4c9773e20
Matt Moriarity <matt@mattmoriarity.com>
2020-05-03 15:00:35 -0600

    Initial Commit

---
+++ Relay/Relay.h
@@ -0,0 +1,19 @@
+//
+//  Relay.h
+//  Relay
+//
+//  Created by Matt Moriarity on 5/3/20.
+//  Copyright © 2020 Matt Moriarity. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Relay.
+FOUNDATION_EXPORT double RelayVersionNumber;
+
+//! Project version string for Relay.
+FOUNDATION_EXPORT const unsigned char RelayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Relay/PublicHeader.h>
+
+

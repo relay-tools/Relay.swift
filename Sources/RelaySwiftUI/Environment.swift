baf3c85eace6092945588a981147e3e4c68e950f
Matt Moriarity <matt@mattmoriarity.com>
2020-05-15 23:45:45 -0600

    Add property wrapper for relay environment

--- Sources/RelaySwiftUI/Environment.swift
+++ Sources/RelaySwiftUI/Environment.swift
@@ -11,3 +11,28 @@ public extension EnvironmentValues {
         set { self[RelayEnvironmentKey.self] = newValue }
     }
 }
+
+public struct WithRelayEnvironment: ViewModifier {
+    let environment: Relay.Environment
+
+    public func body(content: Content) -> some View {
+        content.environment(\.relayEnvironment, environment)
+    }
+}
+
+public extension View {
+    func relayEnvironment(_ environment: Relay.Environment) -> some View {
+        modifier(WithRelayEnvironment(environment: environment))
+    }
+}
+
+@propertyWrapper
+public struct RelayEnvironment: DynamicProperty {
+    @SwiftUI.Environment(\.relayEnvironment) var environment
+
+    public init() {}
+
+    public var wrappedValue: Relay.Environment {
+        environment!
+    }
+}

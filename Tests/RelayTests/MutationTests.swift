439aefb5763c57236e02082d0ee4db75a9025153
Matt Moriarity <matt@mattmoriarity.com>
2020-06-19 22:20:04 -0600

    Add another optimistic update test

--- Tests/RelayTests/MutationTests.swift
+++ Tests/RelayTests/MutationTests.swift
@@ -38,6 +38,50 @@ class MutationTests: XCTestCase {
         assertSnapshot(matching: environment.store.source, as: .recordSource)
     }
 
+    func testBasicNodeUpdateOptimistic() throws {
+        try loadInitialData()
+        assertSnapshot(matching: environment.store.source, as: .recordSource)
+
+        let input = ChangeTodoStatusInput(
+            complete: true, id: "VG9kbzox", userId: "me")
+        let op = ChangeTodoStatusMutation(variables: .init(input: input))
+        let optimisticPayload = [
+            "changeTodoStatus": [
+                "todo": [
+                    "id": "VG9kbzox",
+                    "complete": true,
+                ],
+            ],
+        ]
+
+        let payload = """
+{
+  "errors": [{"message": "This is an error that occurred in the mutation."}],
+}
+"""
+        let parsedPayload = try JSONSerialization.jsonObject(with: payload.data(using: .utf8)!, options: []) as! [String: Any]
+        let advance = environment.delayMockedResponse(op, parsedPayload)
+
+        let publisher = environment.commitMutation(op, optimisticResponse: optimisticPayload)
+        expect(self.environment.store.source["VG9kbzox"]!["complete"] as? Bool).toEventually(beTrue())
+        assertSnapshot(matching: environment.store.source, as: .recordSource)
+
+        advance()
+        var completion: Subscribers.Completion<Error>?
+        waitUntilComplete(publisher.handleEvents(receiveCompletion: { theCompletion in
+            completion = theCompletion
+        }))
+
+        expect(completion).toNot(beNil())
+        guard case .failure(let error) = completion! else {
+            fail("mutation completed successfully when it should have failed")
+            return
+        }
+        expect(error).to(beAKindOf(NetworkError.self))
+
+        assertSnapshot(matching: environment.store.source, as: .recordSource)
+    }
+
     func testDeleteFromListInUpdater() throws {
         try loadInitialData()
         assertSnapshot(matching: environment.store.source, as: .recordSource)

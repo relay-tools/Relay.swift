d184d31aa67c734160836c0275e19dc97915d48f
Matt Moriarity <matt@mattmoriarity.com>
2020-06-19 23:24:47 -0600

    Add test for not publishing snapshot updates for unrelated records

--- Tests/RelayTests/SnapshotPublisherTests.swift
+++ Tests/RelayTests/SnapshotPublisherTests.swift
@@ -42,6 +42,35 @@ class SnapshotPublisherTests: XCTestCase {
         assertSnapshot(matching: snapshot.data, as: .dump)
     }
 
+    func testIgnoreIrrelevantUpdates() throws {
+        try loadInitialData()
+
+        let operation = MoviesTabQuery().createDescriptor()
+        let querySnapshot: Snapshot<MoviesTabQuery.Data?> = environment.lookup(selector: operation.fragment)
+        let listFragmentSnapshot: Snapshot<MoviesList_films.Data?> = environment.lookup(selector: MoviesList_films(key: querySnapshot.data!).selector)
+        var snapshot: Snapshot<MoviesListRow_film.Data?> = environment.lookup(selector: MoviesListRow_film(key: listFragmentSnapshot.data!.allFilms!.edges![0]!.node!).selector)
+        assertSnapshot(matching: snapshot.data, as: .dump)
+
+        var updateCount = 0
+        environment.subscribe(snapshot: snapshot)
+            .sink { newSnapshot in
+                snapshot = newSnapshot
+                updateCount += 1
+            }
+            .store(in: &cancellables)
+
+        var updatedRecordSource = DefaultRecordSource()
+        var record = environment.store.source["ZmlsbXM6Mg=="]!
+        record["director"] = "Someone Else"
+        updatedRecordSource["ZmlsbXM6Mg=="] = record
+
+        environment.store.publish(source: updatedRecordSource)
+        _ = environment.store.notify()
+
+        RunLoop.main.run(until: Date(timeIntervalSinceNow: 0.2))
+        expect(updateCount) == 0
+    }
+
     private func loadInitialData() throws {
         let payload = """
 {

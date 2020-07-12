6561996f98f0426be68ee480ffa12b93d228abaa
Matt Moriarity <matt@mattmoriarity.com>
2020-07-11 22:12:38 -0600

    Remove DataBox from snapshots

--- Sources/Relay/Snapshot.swift
+++ Sources/Relay/Snapshot.swift
@@ -1,43 +1,25 @@
 public struct Snapshot<T>: Equatable {
-    public var data: T { dataBox.get() }
-
-    private var dataBox: DataBox
+    public let data: T
+    private let selectorData: SelectorData?
     public var isMissingData: Bool
     var seenRecords: [DataID: Record]
     public var selector: SingularReaderSelector
 
     init(data: SelectorData?,
-         reify: @escaping (SelectorData?) -> T,
+         reify: (SelectorData?) -> T,
          isMissingData: Bool,
          seenRecords: [DataID: Record],
          selector: SingularReaderSelector) {
-        self.dataBox = DataBox(selectorData: data, reify: reify)
+        self.selectorData = data
+        self.data = reify(data)
         self.isMissingData = isMissingData
         self.seenRecords = seenRecords
         self.selector = selector
     }
 
-    private final class DataBox {
-        let selectorData: SelectorData?
-        let reify: (SelectorData?) -> T
-        var data: T?
-
-        init(selectorData: SelectorData?, reify: @escaping (SelectorData?) -> T) {
-            self.selectorData = selectorData
-            self.reify = reify
-        }
-
-        func get() -> T {
-            if let data = data {
-                return data
-            }
-
-            data = reify(selectorData)
-            return data!
-        }
-    }
-
     public static func ==(lhs: Self, rhs: Self) -> Bool {
-        lhs.dataBox.selectorData == rhs.dataBox.selectorData && lhs.selector.dataID == rhs.selector.dataID && lhs.selector.owner == rhs.selector.owner
+        lhs.selectorData == rhs.selectorData &&
+            lhs.selector.dataID == rhs.selector.dataID &&
+            lhs.selector.owner == rhs.selector.owner
     }
 }

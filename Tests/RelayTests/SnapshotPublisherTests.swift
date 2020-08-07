0959096efc017b58946a6a24126cd7dac1a170cd
Matt Moriarity <matt@mattmoriarity.com>
2020-08-07 12:51:21 -0600

    Add snapshot publisher tests for snapshotting and restoring for optimistic updates

--- Tests/RelayTests/SnapshotPublisherTests.swift
+++ Tests/RelayTests/SnapshotPublisherTests.swift
@@ -72,6 +72,84 @@ class SnapshotPublisherTests: XCTestCase {
         expect(updateCount) == 0
     }
 
+    func testUpdatesForOptimisticUpdate() throws {
+        let op = CurrentUserToDoListQuery()
+        try environment.cachePayload(op, CurrentUserToDoList.myTodos)
+
+        let operation = op.createDescriptor()
+        let selector = SingularReaderSelector(dataID: "VG9kbzox", node: ToDoItem_todo.node, owner: operation.request, variables: operation.request.variables)
+        var snapshot: Snapshot<ToDoItem_todo.Data?> = environment.lookup(selector: selector)
+        assertSnapshot(matching: snapshot.data, as: .dump)
+
+        var updateCount = 0
+        environment.subscribe(snapshot: snapshot)
+            .sink { newSnapshot in
+                snapshot = newSnapshot
+                updateCount += 1
+            }
+            .store(in: &cancellables)
+
+        let mutation = ChangeTodoStatusMutation(input: .init(complete: true, id: "VG9kbzox", userId: "me"))
+        _ = try environment.delayMockedResponse(mutation, ChangeTodoStatus.completeBuyHorse)
+
+        let optimisticPayload = [
+            "changeTodoStatus": [
+                "todo": [
+                    "id": "VG9kbzox",
+                    "complete": true,
+                ],
+            ],
+        ]
+        environment.commitMutation(mutation, optimisticResponse: optimisticPayload)
+            .sink(receiveCompletion: {_ in }) { _ in }
+            .store(in: &cancellables)
+
+        // since we haven't advanced, this will be using the optimistic response
+        expect(updateCount).toEventually(equal(1))
+        assertSnapshot(matching: snapshot.data, as: .dump)
+    }
+
+    func testRevertsOptimisticUpdate() throws {
+        let op = CurrentUserToDoListQuery()
+        try environment.cachePayload(op, CurrentUserToDoList.myTodos)
+
+        let operation = op.createDescriptor()
+        let selector = SingularReaderSelector(dataID: "VG9kbzox", node: ToDoItem_todo.node, owner: operation.request, variables: operation.request.variables)
+        var snapshot: Snapshot<ToDoItem_todo.Data?> = environment.lookup(selector: selector)
+        assertSnapshot(matching: snapshot.data, as: .dump)
+
+        var updateCount = 0
+        environment.subscribe(snapshot: snapshot)
+            .sink { newSnapshot in
+                snapshot = newSnapshot
+                updateCount += 1
+            }
+            .store(in: &cancellables)
+
+        let mutation = ChangeTodoStatusMutation(input: .init(complete: true, id: "VG9kbzox", userId: "me"))
+        let advance = try environment.delayMockedResponse(mutation, ChangeTodoStatus.error)
+
+        let optimisticPayload = [
+            "changeTodoStatus": [
+                "todo": [
+                    "id": "VG9kbzox",
+                    "complete": true,
+                ],
+            ],
+        ]
+        environment.commitMutation(mutation, optimisticResponse: optimisticPayload)
+            .sink(receiveCompletion: {_ in }) { _ in }
+            .store(in: &cancellables)
+
+        // since we haven't advanced, this will be using the optimistic response
+        expect(updateCount).toEventually(equal(1))
+
+        // now we advance so the error happens and we revert the response
+        advance()
+        expect(updateCount).toEventually(equal(2))
+        assertSnapshot(matching: snapshot.data, as: .dump)
+    }
+
     private func loadInitialData() throws {
         let op = MoviesTabQuery()
         try environment.mockResponse(op, MoviesTab.allFilms)

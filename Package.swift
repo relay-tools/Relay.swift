34ed7b2bc7ab0ac98a61586bec0f867be8df0510
Matt Moriarity <matt@mattmoriarity.com>
2020-12-29 08:24:27 -0700

    Upgrade Nimble version

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
     dependencies: [
         .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", .exact("0.50300.0")),
         .package(name: "SnapshotTesting", url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.8.0"),
-        .package(name: "Nimble", url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.0.1")),
+        .package(name: "Nimble", url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "9.0.0")),
     ],
     targets: [
         .target(
